38e8bc2c9627ba15dab02a00b4968b3b63e2d2b7
TERRY WONG <iamtktwong@gmail.com>
2021-01-12 10:07:30 +0800

    code clean

--- src/main/java/com/appswalker/springIntegrationJmsDemo/message/OrderRouter.java
+++ src/main/java/com/appswalker/springIntegrationJmsDemo/message/OrderRouter.java
@@ -1,7 +1,6 @@
 package com.appswalker.springIntegrationJmsDemo.message;
 
 import com.appswalker.springIntegrationJmsDemo.config.IntegrationConstant;
-import com.appswalker.springIntegrationJmsDemo.model.Order;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.Router;
 import org.springframework.messaging.handler.annotation.Header;
@@ -9,7 +8,7 @@ import org.springframework.messaging.handler.annotation.Header;
 @MessageEndpoint
 public class OrderRouter {
     @Router(inputChannel= IntegrationConstant.replies)
-    public String route(Order order, @Header("ORDER_TYPE") String orderType) {
+    public String route(@Header("ORDER_TYPE") String orderType) {
         if ("b2b".equals(orderType)) {
             return "b2b.channel";
         } else if ("b2c".equals(orderType)) {
